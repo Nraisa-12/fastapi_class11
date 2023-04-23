f62d7d106b6296a1a199c93a8411cc8bb67dbece
Noshin Raisa <123591909+Nraisa-12@users.noreply.github.com>
2023-04-23 02:11:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from time import time
+from fastapi import FastAPI, __version__
 
-app = FastAPI()
+app = FastAPI() 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get('/')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
