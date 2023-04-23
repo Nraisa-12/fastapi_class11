616e6a28e84b7868078ea112a43ce7e95e138f4b
Noshin Raisa <123591909+Nraisa-12@users.noreply.github.com>
2023-04-23 13:34:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from time import time
-from fastapi import FastAPI, __version__
+from fastapi import FastAPI
 
-app = FastAPI() 
+app = FastAPI()
 
-@app.get('/')
-async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+
+icd_codes = [
+    {"code": "A00", "description": "Cholera"},
+    {"code": "A01", "description": "Typhoid fever"},
+    {"code": "A02", "description": "Salmonella enteritis"}
+]
+
+cpt_codes = [
+    {"code": "10030", "description": "Guidance for needle placement"},
+    {"code": "99213", "description": "Office or other outpatient visit"},
+    {"code": "92012", "description": "Eye exam and evaluation"}
+]
+
+
+@app.get("/codes")
+def get_codes():
+    return {"icd_codes": icd_codes, "cpt_codes": cpt_codes}
